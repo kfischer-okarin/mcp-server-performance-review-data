bc8ddebf2ba598dacdc53ffc5613f75e80c9003f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-19 12:53:28 +0900

    Add error message and reorganize arguments

--- main.rb
+++ main.rb
@@ -5,26 +5,31 @@ require_relative 'lib/tools'
 
 name 'performance_review_data'
 
+required_env_vars = %w[
+  GITHUB_USERNAME
+  GITHUB_ORGANIZATION
+  GITHUB_PERSONAL_ACCESS_TOKEN
+]
+
+required_env_vars.each do |var|
+  next if ENV[var]
+
+  $stderr.puts "Missing required environment variable: #{var}"
+end
+
 tool 'list_user_pull_request_activity' do
-  description "Retrieve the user's pull request activity (opened and reviewed) from GitHub"
-  argument :username,
-           String,
-           required: true,
-           description: 'The GitHub username to retrieve the activity for'
+  description <<~DESCRIPTION
+    Retrieve the user's pull request activity (opened and reviewed) from GitHub
+  DESCRIPTION
+
   argument :month,
            String,
            required: true,
            description: 'The month to retrieve the activity for (format: YYYY-MM)'
-  argument :organization,
-           String,
-           required: true,
-           description: 'The GitHub organization to retrieve the activity for'
 
   call do |args|
     Tools::ListUserPullRequestActivity.new(
-      username: args[:username],
-      month: args[:month],
-      organization: args[:organization]
+      month: args[:month]
     ).call
   end
 end
